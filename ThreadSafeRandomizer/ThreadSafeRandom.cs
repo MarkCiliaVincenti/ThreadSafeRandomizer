0f322bf5d6b125f5bbac0c42690040a74e11f47b
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-03-14 11:48:04 +0100

    Made class static.

--- ThreadSafeRandomizer/ThreadSafeRandom.cs
+++ ThreadSafeRandomizer/ThreadSafeRandom.cs
@@ -6,7 +6,7 @@ namespace ThreadSafeRandomizer
     /// <summary>
     /// An efficient thread-safe randomizer.
     /// </summary>
-    public class ThreadSafeRandom
+    public static class ThreadSafeRandom
     {
         private static readonly Random _global = new Random();
         private static readonly ThreadLocal<Random> _local = new ThreadLocal<Random>(() =>
